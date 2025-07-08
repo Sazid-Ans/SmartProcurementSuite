a8995520026274933771e8c0a44871aea78838b4
Sazid_Ans <sazid7669584@gmail.com>
2025-07-09 01:03:47 +0530

    Implemented the initail dapper configurations and tested the connection by creating a test controller in vendorService. note:- This commit doest not contains the action method created for testing dapper connection. testing was done in local chnages.

---
+++ VendorService/Controllers/VendorController.cs
@@ -0,0 +1,20 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.Common;
+using VendorService.Models.Dtos;
+
+namespace VendorService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        public IDbConnection _db;
+        public VendorController(IDbConnection db)
+        {
+            _db = db;
+        }
+    }
+}
---
+++ VendorService/Models/Dtos/VendorDto.cs
@@ -0,0 +1,13 @@
+﻿namespace VendorService.Models.Dtos
+{
+    public class VendorDto
+    {
+        public int VendorID { get; set; }                  // int
+        public string CompanyName { get; set; }            // nvarchar
+        public string ContactEmail { get; set; }           // nvarchar
+        public string Phone { get; set; }                  // nvarchar
+        public int Rating { get; set; }                    // int
+        public bool Active { get; set; }                   // bit
+        public DateTime CreatedDate { get; set; }          // datetime2
+    }
+}
