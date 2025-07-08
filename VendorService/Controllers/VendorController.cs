c23b271c4557a19377137e4edb3749652fd92210
Sazid_Ans <sazid7669584@gmail.com>
2025-07-09 02:52:40 +0530

    Implemeted Serilog in Vendor Service and tested in both local gtext file and ms sql server.

--- VendorService/Controllers/VendorController.cs
+++ VendorService/Controllers/VendorController.cs
@@ -12,9 +12,36 @@ namespace VendorService.Controllers
     public class VendorController : ControllerBase
     {
         public IDbConnection _db;
-        public VendorController(IDbConnection db)
+        public ILogger<VendorController> _logger;
+        public VendorController(IDbConnection db, ILogger<VendorController> logger)
         {
             _db = db;
+            _logger = logger;
+        }
+        [HttpGet("/GetAllVendors")]
+        public async Task<IActionResult> Index() 
+        {
+            var sql = "Select * from VendorProfile";
+            _logger.LogInformation("Executing SQL: {Sql}", sql);
+            try
+            {
+                _logger.LogInformation("Trying to Retrive all vendors from the database.");
+
+                var result = await _db.QueryAsync<VendorDto>(sql);
+                if (result == null || !result.Any())
+                {
+                    _logger.LogWarning("No vendors found.");
+                    return NotFound("No vendors found.");
+                }
+
+                _logger.LogInformation("Vendors retrieved successfully.");
+                return Ok(result);
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "An error occurred while retrieving vendors.");
+                return StatusCode(500, "Internal Server Error");
+            }
         }
     }
 }
--- VendorService/Program.cs
+++ VendorService/Program.cs
@@ -1,4 +1,6 @@
 using Microsoft.Data.SqlClient;
+using Serilog;
+using Serilog.Debugging;
 using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,12 +11,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Register Dapper and IDbConnection
 builder.Services.AddScoped<IDbConnection>(sp =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
     var connectionString = config.GetConnectionString("VendorDb");
     return new SqlConnection(connectionString);
 });
+
+//Only for serilog startup errors.
+//SelfLog.Enable(msg =>
+//{
+//    File.AppendAllText("serilog-errors.txt", msg); // log Serilog startup failures
+//});
+
+// Register Serilog for logging
+// Read Serilog config from appsettings.json
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 
